ab246922ca2af408fc7019635c76a762a4a811c0
11Yazan11 <yazanaljundi05@gmail.com>
2025-01-25 12:40:36 +0100

    createdskeleton

---
+++ skeleton.py
@@ -0,0 +1,39 @@
+# IMPORTS
+import math
+#----------
+
+# CONSTANTES ET VARIABLES
+#----------
+
+
+# METHODES ET FONCTIONS DU CODE
+def grille_init():
+    pass
+def affiche_grille():
+    pass
+def colonne_libre():
+    pass
+def place_jeton():
+    pass
+def horizontale():
+    pass
+def verticale():
+    pass
+def diagonale():
+    pass
+def gagne():
+    pass
+def tour_joueur():
+    pass
+def egalite():
+    pass
+def jouer():
+    pass
+#----------
+
+
+# JEU ET BOUCLE DE JEU
+if __name__ == "__main__":
+    while True:
+        pass
+#----------
