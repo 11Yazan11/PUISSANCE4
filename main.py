2dd8937675903c1161bb1e3b2a778487717a827e
11Yazan11 <yazanaljundi05@gmail.com>
2025-01-26 07:31:07 +0100

    NowWeretalking

--- main.py
+++ main.py
@@ -8,12 +8,46 @@ class Main:
         self.wnh = wnh
         self.window = pygame.display.set_mode((wnw, wnh))
         self.running = True
+        self.location = 'WELCOME'
+        self.fps = 60
+        self.clock = pygame.time.Clock()
+
     
     def __drawElems__(self, elems):
         for elem in elems:
             attr = elem['attributes']
             color = elem['color']
-            pygame.draw.rect(self.window, color, (attr.x, attr.y, attr.w, attr.h))
+            if elem['texture'] is None:
+                pygame.draw.rect(self.window, color, (attr.x, attr.y, attr.w, attr.h))
+            else:
+                try:
+                    texture = pygame.image.load(elem['texture']).convert_alpha()
+                    texture = pygame.transform.scale(texture, (attr.w, attr.h))
+                    self.window.blit(texture, (attr.x, attr.y))
+                except:
+                    print('Image file link invalid.')
+
+            if elem['vector'][0] > 0:
+                if elem["attributes"].x >= elem["ogX"] + elem["limit"][0]:
+                    elem['vector'][0] *= -1
+            if elem['vector'][0] < 0:
+                if elem["attributes"].x <= elem["ogX"] - elem["limit"][0]:
+                    elem['vector'][0] *= -1
+            
+            if elem['vector'][1] > 0:
+                if elem["attributes"].y >= elem["ogY"] + elem["limit"][1]:
+                    elem['vector'][1] *= -1
+            if elem['vector'][1] < 0:
+                if elem["attributes"].y <= elem["ogY"] - elem["limit"][1]:
+                    elem['vector'][1] *= -1
+
+            elem["attributes"].x += elem["vector"][0]
+            elem["attributes"].y += elem["vector"][1]
+            
+
+   
+
+
 
     def __render__(self, menu):
         pygame.display.flip()
@@ -21,18 +55,21 @@ class Main:
         self.window.fill(menu_data['Bg'])
         pygame.display.set_caption(f'PROJECT GALAXY MADNESS - {menu_data['Name']}')
         self.__drawElems__(menu_data['Elements'])
+        self.clock.tick(self.fps)
 
     def __listenToEvents__(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.location = 'INGAME'
 
 
 
 
 
 if __name__ == "__main__":
-    GameInstance = Main(1000, 600)
+    GameInstance = Main(900, 600)
     while GameInstance.running:
-        GameInstance.__render__(WELCOMEMENU)
+        GameInstance.__render__(next((menu for menu in ALL_MENUS if menu.__getInfo__()['Name'] == GameInstance.location), None))
         GameInstance.__listenToEvents__()
--- menues.py
+++ menues.py
@@ -6,13 +6,18 @@ class Menu:
         self.bg = bg
         self.all_objects = []
     def __addObject__(self, data, clickable=False):
-        """Excpects a data list with x, y, w, h, c. 
+        """Excpects a data list with req|: x, y, w, h, c, vecxy, lim. 
            w -> width;
            h -> height;
            c -> color;
+           t -> Texture, None if no texture, or link to the image;
+           vecxy -> Tuple, Vector of movement.
+           lim -> Corresponds to a range of movement.
            Renders a 2D menu UI object that can be clickable.
         """
-        self.all_objects.append({"attributes":pygame.Rect(data["x"], data["y"], data["w"], data["h"]), "color":data["c"], "clickable":clickable})
+
+        self.all_objects.append({"ogX":data['x'], "ogY":data['y'],"attributes":pygame.Rect(data["x"], data["y"], data["w"], data["h"]), "color":data["c"], "texture":data["t"], "vector": [data["vecxy"][0], data["vecxy"][1]], "limit":data["lim"],"clickable":clickable})
+
     def __getInfo__(self):
         return {'Id':id(self),'Name':self.name, 'Bg':self.bg, 'Elements':self.all_objects}
     
